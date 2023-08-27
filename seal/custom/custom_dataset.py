b95927af6687922d7be3d50627a1df65e885fa49
haesungpyun <hacastle12@gmail.com>
2023-08-27 22:50:22 +0900

    implement custom dataset

--- seal/custom/custom_dataset.py
+++ seal/custom/custom_dataset.py
@@ -1,8 +1,101 @@
+from ast import Dict
+import codecs
+import fcntl
+import glob
+import json
+import pathlib
 import pickle
+import re
 import numpy as np
 import torch
 from torch.utils.data import DataLoader, Dataset
 from typing import List
+from transformers import AutoTokenizer, PreTrainedTokenizer
+from torch.nn import functional as F
+
+class MyDataset(Dataset):
+    
+    def __init__(
+        self,
+        data_path:str,
+        tokenizer:PreTrainedTokenizer,
+        train:bool = True,
+        label_txt_path:str = None
+        **kwargs
+    ):
+        super().__init__()
+        self.data_path = pathlib.Path(data_path)
+        self.tokenizer = tokenizer
+        self.train = train
+        self.label_txt_path = label_txt_path
+
+        self.label_set = set()
+
+    def read_data(self):
+        for file_ in glob.glob(self.data_path, flags=glob.EXTGLOB):
+            # logger.info(f"Reading {file_}")
+            with open(file_, encoding="utf-8") as f:
+                for line in f:
+                    example = json.loads(line)
+                    instance = self.tokenize_data(**example)
+                    yield instance
+    
+    def tokenize_data(
+        self,
+        title: str,
+        body: str,
+        topics: Dict[str, List[str]],
+        idx: str,
+    ):
+        tokens = self.tokenizer(title, return_tensors='pt', truncation=True, max_length=512)
+        labels_ = []
+        for value in topics.values():
+            labels_ += (
+                [value]  # type:ignore
+                if not isinstance(value, list)
+                else value
+            )
+        self.label_set.update(labels_)
+
+        return {'x': tokens, 'labels':labels_}
+
+    def make_dataset(self):
+        instance_gen_ = self.read_data()
+        
+        self.label_dictionary = {}
+        if self.train:
+            if self.label_txt_path:        
+                _NEW_LINE_REGEX = re.compile(r"\n|\r\n") 
+                filename = self.label_txt_path
+                with codecs.open(filename, "r", "utf-8") as f:
+                    lines = _NEW_LINE_REGEX.split(f.read())
+                    # Be flexible about having final newline or not
+                    if lines and lines[-1] == "":
+                        lines = lines[:-1]
+                    for index, line in enumerate(lines):
+                        token = line.replace("@@NEWLINE@@", "\n")
+                        self.label_dictionary[token] = index
+            else:
+                label_list = list(self.label_set)
+                self.label_dictionary = {label: idx for idx, label in enumerate(label_list)}
+
+            with open(self.data_path.parent + './label_dict.txt', 'w') as f:
+                json.dump(self.label_dictionary)
+   
+        self.total_data = []
+        for tokens, labels_ in instance_gen_:
+            label_idx = []
+            for label in labels_:
+                label_idx.append(self.label_dict[label])
+            self.total_data.append({'x':tokens, 'labels':label_idx})
+    
+    def __len__(self,):
+        return len(self.total_data)
+    
+    def __getitem__(self, idx):
+        return self.total_data[idx]['x'],\
+              self.total_data[idx]['labels']
+
 
 class MNISTDataset(Dataset):
     def __init__(
@@ -27,37 +120,3 @@ class MNISTDataset(Dataset):
     
     def __getitem__(self, idx):
         return self.num_pairs[idx].float(), self.labels[idx].float()
-
-class MyDataset(Dataset):
-    
-    def __init__(
-        self,
-        path_to_data:str,
-        num_labels:int ,
-        **kwargs
-    ):
-        super().__init__()
-        with open(path_to_data, 'rb') as f:
-            self.dataset = pickle.load(f)
-        
-        self.input = self.dataset[0]
-        self.labels = self.dataset[1]
-    
-    def __len__(self,):
-        return len(self.num_pairs)
-    
-    def __getitem__(self, idx):
-        return self.input[idx].float(), self.labels[idx].float()
-
-
-
-
-"""
-train_dataset = MNISTDataset(train_data, test=False)
-val_dataset = MNISTDataset(valid_data, test=False)
-test_dataset = MNISTDataset(test_data, test=True)
-
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
-"""    
--- seal/wrappers/data_constructor.py
+++ seal/wrappers/data_constructor.py
@@ -11,45 +11,27 @@ class DataConstructor(Constructor):
         self.config_dict = config_dict
         
     def make_dataloader(self):
-        dataset = self.construct_class(self.config_dict['dataset_reader'], 'dataset_reader')
-        data_loader = self.construct_class(self.config_dict['data_loader'], 'data_loader', reader=dataset)
+        dataset = self.construct_class(self.config_dict['dataset_reader'], 
+                                       'dataset_reader')
+
+        if "allennlp" in str(self.config_dict['data_loader']):
+            data_loader = self.construct_class(self.config_dict['data_loader'], 
+                                               'data_loader', 
+                                               reader=dataset)
+        else:
+            data_loader = self.construct_class(self.config_dict['data_loader'], 
+                                               'data_loader', 
+                                               dataset=dataset)
         
-        if "allennlp" in str(data_loader.__module__):
+        if "allennlp" in str(self.config_dict['data_loader']):
             instance_generator = (
                 instance
                 for key, data_loader in data_loader.items()
                 for instance in data_loader.iter_instances()
             )
-            vocab = self.construct_class(self.config_dict['vocabulary'],"vocabulary")
+            vocab = self.construct_class(self.config_dict['vocabulary'],"vocabulary", instances=instance_generator)
             data_loader.index_with(vocab)
-        else:
-            self.label_dictionary = {}
-            _NEW_LINE_REGEX = re.compile(r"\n|\r\n")
-            if self.config_dict['vocabulary'].get('filename', False):
-                filename = self.config_dict['vocabulary'].get('filename')
-                with codecs.open(filename, "r", "utf-8") as input_file:
-                    lines = _NEW_LINE_REGEX.split(input_file.read())
-                    # Be flexible about having final newline or not
-                    if lines and lines[-1] == "":
-                        lines = lines[:-1]
-                    for index, line in enumerate(lines):
-                        token = line.replace("@@NEWLINE@@", "\n")
-                        self.label_dictionary[token] = index
-            else:
-                for index, batch in data_loader:
-                    label = batch['label']
-                    if self.label_dictionary.get(label, None):
-                        continue
-                    else:
-                        self.label_dictionary[label] = index
                      
-            tokenizer = self.construct_class(self.config_dict['tokenizer', "tokenizer"])
-            for i, batch in enumerate(data_loader):
-                # 직접 batch tensor를 조작하는 게 맞나...?
-                # dataset/dataloader 단에서 iter 하는 instance들에 적용하는 게 깔끔할 거 같은데
-                batch['x'] = tokenizer(batch['x'], return_tensor='pt')
-                batch['label'] = torch.tensor([self.label_dictionary[i] for i in batch['label']])
-                
         # 패키지 별로 data loading format 동일하게 후처리 wrapping 해주기
 
         return data_loader, vocab
